cd7914d0dff4e525b88271e8d49ab1902ff97028
evalynnsb <43661458+evalynnsb@users.noreply.github.com>
2023-03-02 18:35:33 -0500

    Made common move_servo() that is functional for Create

--- servo.py
+++ servo.py
@@ -1,13 +1,11 @@
 from constants.servos import BackClaw
-from kipr import msleep, enable_servos, set_servo_position, get_servo_position, disable_servos, disable_servo, \
+from kipr import msleep, set_servo_position, get_servo_position, disable_servo, \
     enable_servo
 
 
 def move_servo(new_position, step_time=10):
     servo = new_position.port
     temp = get_servo_position(servo)
-    if servo == BackClaw.port:
-        enable_servo(BackClaw.port)
 
     if temp < new_position:
         while temp < new_position:
@@ -19,6 +17,3 @@ def move_servo(new_position, step_time=10):
             set_servo_position(servo, temp)
             temp -= 5
             msleep(step_time)
-
-    if servo == BackClaw.port:
-        disable_servo(BackClaw.port)
