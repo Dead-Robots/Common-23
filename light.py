dfefa6fa36ddd60bfb0c3d7660bdf0d70bca28e2
KaidenHiggins <kaidenhiggins11@gmail.com>
2023-04-20 20:28:07 -0400

    - added more gyro turns - fixed second cube pickup (it no longer scrapes)
    - Myra, Kaiden

---
+++ light.py
@@ -0,0 +1,58 @@
+from kipr import push_button, msleep, analog
+
+START_LIGHT_THRESHOLD = 0
+
+
+def calibrate(port):
+    global START_LIGHT_THRESHOLD
+    print("Press button with light on")
+    while not push_button():
+        pass
+    while push_button():
+        pass
+    light_on = analog(port)
+    print("On value =", light_on)
+    if light_on > 200:
+        print("Bad calibration")
+        return False
+    msleep(1000)
+    print("Press button with light off")
+    while not push_button():
+        pass
+    while push_button():
+        pass
+    light_off = analog(port)
+    print("Off value =", light_off)
+    if light_off < 3000:
+        print("Bad calibration")
+        return False
+
+    if (light_off - light_on) < 2000:
+        print("Bad calibration")
+        return False
+    START_LIGHT_THRESHOLD = (light_off - light_on) / 2
+    print("Good calibration! ", START_LIGHT_THRESHOLD)
+    return True
+
+
+def wait_4(port):
+    print("waiting for light!! ")
+    while analog(port) > START_LIGHT_THRESHOLD:
+        pass
+
+
+def wait_4_light(port, ignore=False):
+    if ignore:
+        wait_for_button()
+        return
+    while not calibrate(port):
+        pass
+    wait_4(port)
+
+
+def wait_for_button():
+    print('waiting for button')
+    while not push_button():
+        pass
+    while push_button():
+        pass
