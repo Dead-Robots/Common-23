3df352e428628d3e777219413ba411a158b96651
TomColeman <tjctops@gmail.com>
2023-07-01 12:50:59 -0400

    Made the MultitaskedMotor class to manage the rake motor.

--- multitasker.py
+++ multitasker.py
@@ -1,4 +1,6 @@
+import time
 from threading import Thread
+from kipr import get_motor_position_counter, motor_power, freeze
 
 
 class Multitasker:
@@ -15,7 +17,24 @@ class Multitasker:
 
     def do(self, function, args=None):
         # create a mew thread, store it, and start it
-        thread = Thread(target=function, args=args, daemon=True)
+        thread = Thread(target=function, args=args or (), daemon=True)
         self.threads.append(thread)
         thread.start()
         return thread
+
+
+class MultitaskedMotor:
+    def __init__(self, port, position):
+        self.port = port
+        self.position = position
+        self.running = True
+        self.thread = Thread(target=self._move_motors, daemon=True)
+        self.thread.start()
+
+    def _move_motors(self):
+        while self.running:
+            current = get_motor_position_counter(self.port)
+
+            motor_power(self.port, max(min(int(100 * (self.position - current) / 300), 100), -100))
+            time.sleep(0.1)
+        freeze(self.port)
